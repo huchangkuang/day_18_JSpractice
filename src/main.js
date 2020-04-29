122f07ba35e5743c5e7ff6ccae799f5835c418e7
huchangkuang <603021137@qq.com>
2020-04-29 16:34:47 +0800

    实现间隔显示一段字符串

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 let demo = document.querySelector("#demo")
-let n = 1
+let string = `
+大家好，我是一名前端新人
+`
+let n = 0
 
-demo.innerHTML = n
+// demo.innerHTML = string
 let step = () => {
     setTimeout(() => {
+        demo.innerHTML = string[n]
         n += 1
-        demo.innerHTML = n
-        step()
-    }, 1000)
+        if (n < string.length) {
+            step()
+        }
+    }, 300)
 }
 step()
