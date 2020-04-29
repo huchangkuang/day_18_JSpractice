8ac0cd6e09d92381a2d8241bbec450c20e945284
huchangkuang <603021137@qq.com>
2020-04-29 21:08:39 +0800

    实现换行

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
 let demo = document.querySelector("#demo")
 let string = `
-大家好，我是一名前端新人
+你好，我是一名前端新人
+接下来我要加样式了
+我要加的样式是
+body{
+    color:red;
+}
 `
+let string2 = ""
+
+// string = string.replace(/\n/g,"<br>")
 let n = 0
 
-// demo.innerHTML = string
+// demo.innerHTML = string.substring(0,n)
 let step = () => {
     setTimeout(() => {
-        demo.innerHTML = string[n]
+        string[n] === "\n" ? string2 += "<br>" : string2 += string[n]
+        // console.log(n)
+        demo.innerHTML = string2
         n += 1
         if (n < string.length) {
             step()
         }
-    }, 300)
+    }, 100)
 }
 step()
